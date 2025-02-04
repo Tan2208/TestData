521b72ce31639e62103a1db5e4dcc97e4248988b
Tan2208 <86584383+Tan2208@users.noreply.github.com>
2025-02-04 17:45:52 +0700

    Creat basic Exception and add Validation for user

--- src/main/java/com/example/TestData/controller/UserController.java
+++ src/main/java/com/example/TestData/controller/UserController.java
@@ -4,7 +4,9 @@ import com.example.TestData.dto.request.UserCreationRequest;
 import com.example.TestData.dto.request.UserUpdateRequest;
 import com.example.TestData.entity.User;
 import com.example.TestData.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,7 +18,7 @@ public class UserController {
     private UserService userService;
 
     @PostMapping
-    User createUser(@RequestBody UserCreationRequest request) {
+    User createUser(@RequestBody @Valid UserCreationRequest request) {
         return userService.createRequest(request);
     }
 
--- src/main/java/com/example/TestData/dto/request/UserCreationRequest.java
+++ src/main/java/com/example/TestData/dto/request/UserCreationRequest.java
@@ -1,9 +1,14 @@
 package com.example.TestData.dto.request;
 
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 
 public class UserCreationRequest {
+    @Size(min = 3, message = "Username must be at least 3 characters")
     private String username;
+
+    @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
     private String lastname;
     private String firstname;
